3b44e7e8a6c6ba1498b0707f75478abcf94330f9
karolb <karol.brejna@poczta.onet.pl>
2013-01-29 09:02:02 +0100

    Dodanie źródeł drugiego context listener'a - własna konfiguracja puli połączeń.

---
+++ ParallelWeb/src/pl/atena/technoblog/parallel/MyListener2.java
@@ -0,0 +1,40 @@
+package pl.atena.technoblog.parallel;
+
+import java.util.concurrent.*;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebListener;
+
+//@WebListener
+public class MyListener2 implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("Servlet Context initialized.3");
+        final int numberOfThreads = 1;
+        final ExecutorService threadPool = new ThreadPoolExecutor(
+                numberOfThreads, // core thread pool size
+                numberOfThreads, // maximum thread pool size
+                0L, TimeUnit.MILLISECONDS,// time to wait before resizing pool
+                new LinkedBlockingQueue<Runnable>(numberOfThreads),
+                new ThreadPoolExecutor.CallerRunsPolicy());
+    
+        final ServletContext servletContext = sce.getServletContext();
+        servletContext.setAttribute("myExecutorService", threadPool);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("Servlet Context destroyed.");
+        final ExecutorService threadPool = (ExecutorService) sce
+                .getServletContext().getAttribute("myExecutorService");
+        // stop accepting new tasks
+        threadPool.shutdown();
+        try {
+            // wait for task to terminate
+            threadPool.awaitTermination(1, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            // enough waiting!
+            threadPool.shutdownNow();
+        }
+    }
+}
